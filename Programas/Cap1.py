7fcc80beb0eae54423da84f7ff098e893685e943
Rafael Martinez <raf_neta@hotmail.com>
2017-12-18 00:02:25 -0600

    Creamos Paquete  de programas

---
+++ Programas/Cap1.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from Programas.nuco import *
+from Programas.nuscos import *
+from Programas.nc import *
+from Programas.ncconjugado import *
+from Programas.ncinversomul import *
+from Programas.ncinversoadi import *
+from Programas.ncsuma import *
+from Programas.ncresta import *
+from Programas.ncproducto import *
+from Programas.ncdivision import *
+
